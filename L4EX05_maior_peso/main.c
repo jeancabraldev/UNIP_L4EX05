bfe1ebe70ff359d053a0c53c96fd3d0548deb562
jeancabraldev <54410614+jeancabraldev@users.noreply.github.com>
2019-08-25 09:15:53 -0300

    Add files via upload

---
+++ L4EX05_maior_peso/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float PesoPacoteMaior, pesoPacote;
+    int i;
+
+    for(i = 1; i <= 10; i++){
+        printf("\nInforme o peso do %d pacote: ", i);
+        scanf("%f", &pesoPacote);
+        if(pesoPacote > PesoPacoteMaior){
+            PesoPacoteMaior = pesoPacote;
+        }
+    }
+    printf("\nO pacote mais pesado e %.2f: \n", PesoPacoteMaior);
+    return 0;
+}
